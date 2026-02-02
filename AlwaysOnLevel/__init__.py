28bf174166c85c206e91804dfc662a285b41abe6
EdricY <yu.edric@gmail.com>
2026-02-02 14:24:08 -0500

    create always on level mod

---
+++ AlwaysOnLevel/__init__.py
@@ -0,0 +1,19 @@
+import unrealsdk.unreal as unreal
+from mods_base import build_mod, get_pc, hook
+from unrealsdk.hooks import Block, prevent_hooking_direct_calls
+
+@hook("WillowGame.WillowPawn:SetGameStage")
+def set_game_stage(self, caller: unreal.UObject, function: unreal.UFunction, params: unreal.WrappedStruct):
+  print(f"set_game_stage {caller.NewGameStage}")
+  print(self)
+  if self.Class.Name == "WillowPlayerPawn":
+    return None
+
+  level = get_pc().PlayerReplicationInfo.ExpLevel
+
+  with prevent_hooking_direct_calls():
+    params(level)
+
+  return Block
+
+build_mod(hooks=[set_game_stage])
