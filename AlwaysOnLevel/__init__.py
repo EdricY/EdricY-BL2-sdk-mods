f8c9977a781ae44ac4e0bb113d5c2ea9256356eb
EdricY <yu.edric@gmail.com>
2026-05-01 16:02:49 -0400

    save christmas! avoid downleveling interactive objects in xmas area

--- AlwaysOnLevel/__init__.py
+++ AlwaysOnLevel/__init__.py
@@ -1,5 +1,5 @@
 import unrealsdk.unreal as unreal
-from mods_base import build_mod, get_pc, hook, SpinnerOption
+from mods_base import build_mod, get_pc, hook, SpinnerOption, BoolOption
 from unrealsdk.hooks import Block, prevent_hooking_direct_calls
 
 @hook("WillowGame.WillowPawn:SetGameStage")
@@ -16,6 +16,11 @@ def set_game_stage(obj: unreal.UObject, args: unreal.WrappedStruct, ret, func: u
     arg_level = args.NewInventoryGameStage
   
   level = get_pc().PlayerReplicationInfo.ExpLevel
+
+  # edge case: don't down-level interactive objects in xmas area. it ruins christmas.
+  if oid_xmas.value and arg_level > level and oid_dir.value != "Up" and str(obj).startswith("WillowInteractiveObject'Xmas_P.TheWorld"):
+    return
+
 
   with prevent_hooking_direct_calls():
     if oid_dir.value == "Both":
@@ -39,12 +44,20 @@ oid_dir = SpinnerOption(
     ["Down", "Up", "Both"],
     True,
     description=("Should Enemies be leveled up only, down only, or both"
-                "\nDown = Higher level enemies will be lowered to your level, lower level enemies will be untouched."
-                "\nUp = Lower level enemies will be brought up to your level, higher level enemies will be untouched."
+                "\nDown = Higher level enemies will be lowered to your level."
+                "\nUp = Lower level enemies will be brought up to your level"
+    ),
+)
+
+oid_xmas = BoolOption(
+    identifier="Don't Ruin Christmas",
+    value=True,
+    description=(
+        "Avoid leveling down objects in Marcus Mercenary Day to save christmas! (Enemies handled normally)"
     ),
 )
 
 build_mod(
   hooks=[set_game_stage],
-  options=[oid_dir]
+  options=[oid_dir, oid_xmas]
 )
